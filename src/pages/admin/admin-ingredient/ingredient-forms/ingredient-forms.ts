bdde7b6ad59cd93d98eeea4f6ab1fd92900fe060
DESKTOP-04IIIN0\Malik <malik.verdiere@gmail.com>
2017-11-26 18:05:15 +0100

    firstuuu

--- src/pages/admin/admin-ingredient/ingredient-forms/ingredient-forms.ts
+++ src/pages/admin/admin-ingredient/ingredient-forms/ingredient-forms.ts
@@ -2,24 +2,49 @@ import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 import {IngredientService} from '../../../../providers/ingredient-service/ingredient-service'
 import {Ingredient} from '../../../../model/Ingredient'
+import { Validators, FormGroup, FormControl } from '@angular/forms';
 @Component({
   selector: 'page-ingredient-forms',
   templateUrl: 'ingredient-forms.html'
 })
-export class IngredientFormPage {
 
-  ingredientList: Ingredient[];
-  constructor(public navCtrl: NavController, public ingredientService: IngredientService) {
-
+export class IngredientFormPage{
+  
+    public form:FormGroup;
+    private ingredient:Ingredient;
+    private update:boolean;
+    constructor(private ingredientService :IngredientService) { }
+  
+  
+    ionViewDidLoad() {
+      //let id = this.route.snapshot.params['id'];
+      this.update = false;
+  
+      this.form = new FormGroup({
+        name: new FormControl('', Validators.required),
+        weight: new FormControl('', Validators.required),
+        price: new FormControl('', Validators.required, Validators.pattern[0-9]),
+        });
+  
+      // if(id != null){
+      //   this.update = true,
+      //   this.ingredientService.getById(id).then( data => {
+      //     this.ingredient = data;
+      //     this.form.patchValue({
+      //       name: data.name,
+      //       weight: data.weight,
+      //       price: data.price,
+      //     });
+      //   });
+      // }
+    }
+  
+    onSubmit(){
+       if(this.update){
+        // this.ingredientService.update(this.form.value, this.contact.id);
+       }else{
+          this.ingredientService.create(this.form.value).subscribe(data => {
+          });
+      }
+    }
   }
-
-  ionViewDidLoad() {
-
-    console.log('ionViewDidLoad DisplayPizzaPage');
-    this.ingredientService.get().then((data: Array<Ingredient>) =>{
-        this.ingredientList = data;
-    });
-    
-    console.log(this.ingredientService);
-  }
-}
