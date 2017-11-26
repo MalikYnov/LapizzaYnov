704452fb5888c4c421e832ebc2b8f27126c66986
DESKTOP-04IIIN0\Malik <malik.verdiere@gmail.com>
2017-11-26 22:52:17 +0100

    firstuu

--- src/pages/display-pizza/display-pizza.ts
+++ src/pages/display-pizza/display-pizza.ts
@@ -2,6 +2,9 @@ import { Component } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
 import {PizzaService} from '../../providers/pizza-service/pizza-service'
 import {Pizza} from '../../model/Pizza'
+import { AlertController } from 'ionic-angular';
+import { Storage } from '@ionic/storage';
+import { ToastController } from 'ionic-angular';
 /**
  * Generated class for the DisplayPizzaPage page.
  *
@@ -17,13 +20,48 @@ export class DisplayPizza{
 
   pizza:Pizza;
   test:string;
-  constructor(public navCtrl: NavController, public navParams: NavParams, public pizzaService:PizzaService) {
+  constructor(public navCtrl: NavController, public navParams: NavParams, public pizzaService:PizzaService, public alertCtrl: AlertController,private toastCtrl:ToastController,private storage: Storage,) {
     console.log('Constructr');
+    this.pizza = null;
   }
 
   ionViewDidLoad() {
     let id = this.navParams.get('id');
     this.pizza = this.pizzaService.getByid(id);
+  }
+  
+
+  addPizza(pizza){
+    let confirm = this.alertCtrl.create({
+      title: 'Ajouter la pizza',
+      message: 'Do you agree to use this lightsaber to do good across the intergalactic galaxy?',
+      buttons: [
+        {
+          text: 'non',
+          handler: () => {
+          }
+        },
+        {
+          text: 'Agree',
+          handler: () => {
+            this.storage.set('pizza', pizza.name);
+            let toast = this.toastCtrl.create({
+              message: 'la pizza à bien était ajouté',
+              duration: 3000,
+              position: 'bottom'
+            });
+          
+            toast.onDidDismiss(() => {
+              console.log('Dismissed toast');
+            });
+          
+            toast.present();
+          }
+        }
+      ]
+    });
+    confirm.present();
+
   }
 
 }
